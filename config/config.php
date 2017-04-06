fd2bb24f0bca82358850c740a4832350b987f9e6
Clément Caira <clement.caira@supinternet.fr>
2017-04-06 17:22:32 +0200

    first commit mvc

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$routes = [
+    'home'     => 'default',
+    'about'    => 'default',
+    'contact'  => 'default',
+    'register' => 'user',
+    'login'    => 'user',
+    'logout'   => 'user',
+];
+
+$db_config = [
+    'name' => 'mvc',
+    'host' => 'localhost',
+    'user' => 'mvc',
+    'pass' => 'mvcpass'
+];
---
+++ views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Home page</title>
+        <link rel="stylesheet" href="web/style.css">
+    </head>
+    <body>
+        Hello <?php echo $username ?>, this is an awesome template. Master branch is master.
+    </body>
+</html>
