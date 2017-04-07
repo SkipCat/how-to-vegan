5a5cd05221bcf9815d8a84c1b195e9e083a80064
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-04-07 23:13:25 +0200

    get and display all values of form!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,31 @@
 $(function() {
     var buttonAddProduct = $('#add-product');
     var product = $('select');
-    shoppingList = [];
+    var shoppingList = [];
 
     // add new product clicking on button
     buttonAddProduct.on('click', function() { // error if buttonAddProduct
         var newProduct = product.clone().insertBefore(this); // insert select before button
         newProduct.after('<br>'); // add space after new select
-        console.log($('select'));
+        newProduct.each(function() {
+            $(this).on('click', function() {
+                if ($(this).val() != 'NULL') {
+                    shoppingList.push($(this).val());
+                    console.log($(this).val());
+                    console.log(shoppingList);
+                }
+            });
+        });
     });
-
-/*
-    // add newProduct clicking on product
-    $('select').on('click', function() { 
-        var newProduct = product.clone().insertAfter(this);
-        newProduct.after('<br>');
-    });
-*/
-
-/*
+    
     $('select').each(function() {
         $(this).on('click', function() {
             if ($(this).val() != 'NULL') {
                 shoppingList.push($(this).val());
                 console.log($(this).val());
+                console.log(shoppingList);
             }
         });
-    });
-*/
-
-    $('select').on('click', function() {
-        if ($(this).val() != 'NULL') {
-            shoppingList.push($(this).val());
-            console.log($(this).val());
-        }
     });
 
     $('form').on('submit', function() {
@@ -41,5 +33,13 @@ $(function() {
         $('#recap-list').text(shoppingList);
         return false;
     });
+
+    /*
+    // add newProduct clicking on product
+    $('select').on('click', function() { 
+        var newProduct = product.clone().insertAfter(this);
+        newProduct.after('<br>');
+    });
+    */
 
 });
