541c7aa47b480ece37774564e2bdd2c1f949b58f
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-04-07 11:28:36 +0200

    form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,23 @@ window.onload = function() {
     var product = document.querySelectorAll("select");
     var listProducts = [];
     var buttonAdProduct = document.querySelector("#add-product");
+    console.log(form, product, buttonAdProduct);
 
     for (var i = 0; i < product.length; i ++) {
-        listProducts.push(product[i].value);
+        if (product[i].value != "NULL") {
+            console.log('troll');
+            form.insertBefore(product[0], buttonAdProduct);
+        }
         console.log(product[i].value);
     }
 
+/*
     buttonAdProduct.onclick = function(event) {
         event.preventDefault();
         var newProduct = form.insertBefore(product[0], buttonAdProduct);
         return false;
     };
+*/
 
     form.onsubmit = function() {
         console.log('form submitted');
