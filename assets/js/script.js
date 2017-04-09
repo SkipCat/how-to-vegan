32ce811eaa5d4dab7a77a3c6422291144ed86e73
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-04-09 11:59:16 +0200

    check if value already exists, FORM FINISHED

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,66 @@ $(function() {
     var buttonAddProduct = $('#add-product');
     var product = $('select');
     var shoppingList = [];
+    var alreadyExist = []; 
+
+    var filter = $('input[type="checkbox"]');
+    var listFilters = [];
+
+    // get chosen filters
+    filter.each(function() {
+        $(this).click(function() {
+            if ($(this).is(':checked') == true) {
+                listFilters.push($(this).val());
+            }
+        });
+    });
+    // remove unchecked filters from list
 
     // add new product clicking on button
-    buttonAddProduct.on('click', function() { // error if buttonAddProduct
+    buttonAddProduct.on('click', function() {
         var newProduct = product.clone().insertBefore(this); // insert select before button
         newProduct.after('<br>'); // add space after new select
+
+        // for new selects
         newProduct.each(function() {
             $(this).on('click', function() {
                 if ($(this).val() != 'NULL') {
-                    shoppingList.push($(this).val());
-                    console.log($(this).val());
-                    console.log(shoppingList);
+                    for (var i = 0; i < shoppingList.length; i ++) {
+                        alreadyExist.push(shoppingList[i]);
+                    }
+
+                    if ($.inArray($(this).val(), alreadyExist) == -1) { // product not found in array
+                        shoppingList.push($(this).val());
+                        console.log('list:', shoppingList, shoppingList.length);
+                    }
+                    else {
+                        console.log('product null or already selected');
+                    }
                 }
             });
         });
     });
     
-    $('select').each(function() {
+    // for 1rst select
+    product.each(function() {
         $(this).on('click', function() {
-            if ($(this).val() != 'NULL') {
+            if ($(this).val() != 'NULL' && shoppingList.length == 0) {
                 shoppingList.push($(this).val());
-                console.log($(this).val());
-                console.log(shoppingList);
             }
         });
     });
 
     $('form').on('submit', function() {
-        $('form').slideUp(); // $('form').hide();
-        $('#recap-list').text(shoppingList);
+        $('form').slideUp(); // OR $('form').hide();
+
+        if (listFilters.length > 0) { // display filters and shopping
+            $('#recap-list').html('Your shopping list:<br>' + shoppingList + '<br><br>with filters:<br>' + listFilters); 
+        }
+        else { // display only shopping list
+            $('#recap-list').html('Your shopping list:<br>' + shoppingList);
+        }
+
         return false;
     });
-
-    /*
-    // add newProduct clicking on product
-    $('select').on('click', function() { 
-        var newProduct = product.clone().insertAfter(this);
-        newProduct.after('<br>');
-    });
-    */
 
 });
