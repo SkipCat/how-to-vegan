da1c308b2ce04992d6b5ce2e72185eafab545193
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-04-15 19:19:39 +0200

    install grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,6 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		/*
-		concat: {
-			prod: {
-				files: {
-					'assets/css/build.scss': 'assets/css/*.scss'
-				}
-			}
-			dist: {
-				src: ['assets/css/*.scss'],
-				dest: 'assets/css/build.scss',
-			}
-		},*/
 		watch: {
 			css: {
 				files: 'assets/css/style.scss',
@@ -29,22 +17,13 @@ module.exports = function(grunt) {
 				}
             }
         },
-		uglify: {
-			prod: {
-				files: {
-					'assets/js/script.min.js': ['assets/js/*.js']
-				}
-			}
-		}
 	});
     
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
 		'watch',
-		'sass',
-		'uglify'
+		'sass'
 	]);
 };
