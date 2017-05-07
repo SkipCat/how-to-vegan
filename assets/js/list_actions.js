eda43eabd502f0644c7fc5d0433dfaab84ef33d6
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-05-07 21:46:02 +0200

    all views of convert page done

---
+++ assets/js/list_actions.js
@@ -0,0 +1,11 @@
+// pin/delete/keep list
+
+window.onload = function() {
+    var pinButton = document.querySelector('#img-pin');
+    var deleteButton = document.querySelector('#img-delete');
+    var keepButton = document.querySelector('#img-list-keep');
+
+    keepButton.onclick = function() {
+        
+    };
+};
