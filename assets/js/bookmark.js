9f971d3e8ece4aca07c688c5836f2c12a83ecb28
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-05-10 23:34:14 +0200

    finish home page integration

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -1,15 +1,16 @@
 function getCurrentPage(currentPage) {
-    console.log('in page function');
     var currentPage = window.location.href;
     var convertLink = document.querySelector('#convert-page');
     var basketLink = document.querySelector('#basket-page');
     var recipeLink = document.querySelector('#recipe-page');
     var aboutLink = document.querySelector('#about-page');
 
+    if (currentPage.search('home') !== -1) {
+        document.querySelector('header').id = 'header-home';
+    }
     if (currentPage.search('convert') !== -1) {
         convertLink.id = 'active-page';
         document.querySelector('header').id = 'header-convert';
-        console.log('convert');
     }
     if (currentPage.search('basket') !== -1) {
         basketLink.id = 'active-page';
@@ -18,10 +19,6 @@ function getCurrentPage(currentPage) {
     if (currentPage.search('recipe') !== -1) {
         recipeLink.id = 'active-page';
         document.querySelector('header').id = 'header-recipe';
-    }
-    if (currentPage.search('profile') !== -1) {
-        aboutLink.id = 'active-page';
-        //document.querySelector('header').id = 'header-profile';
     }
     if (currentPage.search('about') !== -1) {
         aboutLink.id = 'active-page';
--- assets/js/map/jquery.googlemap.js
+++ assets/js/map/jquery.googlemap.js
@@ -1,360 +1,67 @@
-$(function() {
+var paris = {lat: -33.91721, lng: 151.22630};
 
-	$.fn.googleMap = function(params) {
-		params = $.extend( {
-			zoom : 10,
-			coords : [48.895651, 2.290569],
-			type : "ROADMAP",
-			debug : false,
-			langage : "english",
-			overviewMapControl: false,
-			streetViewControl: false,
-			scrollwheel: false,
-			mapTypeControl: false
-		}, params);
+var markers = [
+    {
+        "title": 'Aksa Beach',
+        "lat": '19.1759668',
+        "lng": '72.79504659999998',
+        "description": 'Aksa Beach is a popular beach and a vacation spot in Aksa village at Malad, Mumbai.'
+    },
+    {
+        "title": 'Juhu Beach',
+        "lat": '19.0883595',
+        "lng": '72.82652380000002',
+        "description": 'Juhu Beach is one of favourite tourist attractions situated in Mumbai.'
+    },
+    {
+        "title": 'Girgaum Beach',
+        "lat": '18.9542149',
+        "lng": '72.81203529999993',
+        "description": 'Girgaum Beach commonly known as just Chaupati is one of the most famous public beaches in Mumbai.'
+    },
+    {
+        "title": 'Jijamata Udyan',
+        "lat": '18.979006',
+        "lng": '72.83388300000001',
+        "description": 'Jijamata Udyan is situated near Byculla station is famous as Mumbai (Bombay) Zoo.'
+    },
+    {
+        "title": 'Sanjay Gandhi National Park',
+        "lat": '19.2147067',
+        "lng": '72.91062020000004',
+        "description": 'Sanjay Gandhi National Park is a large protected area in the northern part of Mumbai city.'
+    }
+];
+window.onload = function () {
+    LoadMap();
+}
+function LoadMap() {
+    var mapOptions = {
+        center: paris,
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-		switch(params.type) {
-			case 'ROADMAP':
-			case 'SATELLITE':
-			case 'HYBRID':
-			case 'TERRAIN':
-				params.type = google.maps.MapTypeId[params.type];
-				break;
-			default:
-				params.type = google.maps.MapTypeId.ROADMAP;
-				break;
-		}
+    //Create and open InfoWindow.
+    var infoWindow = new google.maps.InfoWindow();
 
-		this.each(function() {
+    for (var i = 0; i < markers.length; i++) {
+        var data = markers[i];
+        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: data.title
+        });
 
-			var map = new google.maps.Map(this, {
-				zoom: params.zoom,
-				center: new google.maps.LatLng(params.coords[0], params.coords[1]),
-				mapTypeId: params.type,
-				scrollwheel: params.scrollwheel,
-				streetViewControl: params.streetViewControl,
-				overviewMapControl: params.overviewMapControl,
-				mapTypeControl: params.mapTypeControl
-
-			});
-
-			$(this).data('googleMap', map);
-			$(this).data('googleLang', params.langage);
-			$(this).data('googleDebug', params.debug);
-			$(this).data('googleMarker', new Array());
-			$(this).data('googleBound', new google.maps.LatLngBounds());
-		});
-
-		return this;
-	}
-
-	$.fn.addMarker = function(params) {
-		params = $.extend( {
-			coords : false,
-			address : false,
-			url : false,
-			id : false,
-			icon : false,
-			draggable : false,
-			title : "",
-			text : "",
-			success : function() {}
-		}, params);
-
-		this.each(function() {
-			$this = $(this);
-
-			if(!$this.data('googleMap')) {
-				if($this.data('googleDebug'))
-					console.error("jQuery googleMap : Unable to add a marker where there is no map !");
-					
-				return false;
-			}
-
-			if(!params.coords && !params.address) {
-				if($this.data('googleDebug'))
-					console.error("jQuery googleMap : Unable to add a marker if you don't tell us where !");
-					
-				return false;
-			}
-
-			if(params.address && typeof params.address == "string") {
-
-				var geocodeAsync = function($that) {
-
-					var geocoder = new google.maps.Geocoder();
-
-					geocoder.geocode({
-						address : params.address,
-						bounds : $that.data('googleBound'),
-						language : $that.data('googleLang')
-					}, function(results, status) {
-
-						if (status == google.maps.GeocoderStatus.OK) {
-							$that.data('googleBound').extend(results[0].geometry.location);
-
-							if(params.icon) {
-								var marker = new google.maps.Marker({
-									map: $this.data('googleMap'),
-									position: results[0].geometry.location,
-									title: params.title,
-									icon: params.icon,
-									draggable: params.draggable
-								});
-							} else {
-								var marker = new google.maps.Marker({
-									map: $that.data('googleMap'),
-									position: results[0].geometry.location,
-									title: params.title,
-									draggable: params.draggable
-								});
-							}
-
-							if(params.draggable) {
-								google.maps.event.addListener(marker, 'dragend', function() {
-									var location = marker.getPosition();
-
-									var coords = {};
-
-									coords.lat = location.lat();
-									coords.lon = location.lng();
-
-									params.success(coords, $this);
-								});
-							}
-
-							if(params.title != "" && params.text != "" && !params.url) {
-								var infowindow = new google.maps.InfoWindow({
-									content: "<h1>"+params.title+"</h1>"+params.text
-								});
-
-								var map = $that.data('googleMap');
-
-								google.maps.event.addListener(marker, 'click', function() {
-									infowindow.open(map, marker);
-								});
-							} else if(params.url) {
-								google.maps.event.addListener(marker, 'click', function() {
-									document.location = params.url;
-								});
-							}
-
-							if(!params.id) {
-								$that.data('googleMarker').push(marker);
-							} else {
-								$that.data('googleMarker')[params.id] = marker;
-							}
-
-							if($that.data('googleMarker').length == 1) {
-								$that.data('googleMap').setCenter(results[0].geometry.location);
-								$that.data('googleMap').setZoom($that.data('googleMap').getZoom());
-							} else {
-								$that.data('googleMap').fitBounds($that.data('googleBound'));
-							}
-
-							var coords = {};
-							coords.lat = results[0].geometry.location.lat();
-							coords.lon = results[0].geometry.location.lng();
-
-							params.success(coords, $this);
-
-						} else {
-							if($this.data('googleDebug'))
-								console.error("jQuery googleMap : Unable to find the place asked for the marker ("+status+")");
-						}
-					});
-				}($this);
-			} else {
-				$this.data('googleBound').extend(new google.maps.LatLng(params.coords[0], params.coords[1]));
-
-        			if(params.icon) {
-					var marker = new google.maps.Marker({
-						map: $this.data('googleMap'),
-						position: new google.maps.LatLng(params.coords[0], params.coords[1]),
-						title: params.title,
-						icon: params.icon,
-						draggable: params.draggable
-					});
-				} else {
-					var marker = new google.maps.Marker({
-						map: $this.data('googleMap'),
-						position: new google.maps.LatLng(params.coords[0], params.coords[1]),
-						title: params.title,
-						draggable: params.draggable
-					});
-				}
-
-        			if(params.title != "" && params.text != "" && !params.url) {
-          				var infowindow = new google.maps.InfoWindow({
-						content: "<h1>"+params.title+"</h1>"+params.text
-					});
-
-					var map = $this.data('googleMap');
-
-	        			google.maps.event.addListener(marker, 'click', function() {
-		        			infowindow.open(map, marker);
-	        			});
-				} else if(params.url) {
-          				google.maps.event.addListener(marker, 'click', function() {
-              					document.location = params.url;
-        				});
-				}
-
-				if(params.draggable) {
-					google.maps.event.addListener(marker, 'dragend', function() {
-						var location = marker.getPosition();
-
-						var coords = {};
-
-						coords.lat = location.lat();
-						coords.lon = location.lng();
-
-						params.success(coords, $this);
-					});
-				}
-
-				if(!params.id) {
-       					$this.data('googleMarker').push(marker);
-        			} else {
-        				$this.data('googleMarker')[params.id] = marker;
-        			}
-
-				if($this.data('googleMarker').length == 1) {
-					$this.data('googleMap').setCenter(new google.maps.LatLng(params.coords[0], params.coords[1]));
-					$this.data('googleMap').setZoom($this.data('googleMap').getZoom());
-				} else {
-					$this.data('googleMap').fitBounds($this.data('googleBound'));
-				}
-
-				params.success({
-					lat: params.coords[0],
-					lon: params.coords[1]
-				}, $this);
-			}
-		});
-
-		return this;
-	}
-
-	$.fn.removeMarker = function(id) {
-		this.each(function() {
-			var $this = $(this);
-
-    			if(!$this.data('googleMap')) {
-    				if($this.data('googleDebug'))
-      					console.log("jQuery googleMap : Unable to delete a marker where there is no map !");
-      					
-      				return false;
-    			}
-
-    			var $markers = $this.data('googleMarker');
-
-    			if(typeof $markers[id] != 'undefined') {
-    				$markers[id].setMap(null);
-    				
-      				if($this.data('googleDebug'))
-      					console.log('jQuery googleMap : marker deleted');
-      					
-      				return true;
-    			} else {
-      				if($this.data('googleDebug'))
-      					console.error("jQuery googleMap : Unable to delete a marker if it not exists !");
-      		
-      				return false;
-    			}
-		});
-	}
-
-	$.fn.addWay = function(params) {
-		params = $.extend( {
-			start : false,
-			end : false,
-			step : [],
-			route : false,
-			langage : 'english'
-		}, params);
-
-		var direction = new google.maps.DirectionsService({
-			region: "fr"
-		});
-
-		var way = new google.maps.DirectionsRenderer({
-			draggable: true,
-			map: $(this).data('googleMap'),
-			panel: document.getElementById(params.route),
-			provideTripAlternatives: true
-		});
-		
-		document.getElementById.innerHTML = "";
-
-		var waypoints = [];
-
-    		for(var i in params.step) {
-    			var step;
-      			if(typeof params.step[i] == "object") {
-        			step = new google.maps.LatLng(params.step[i][0], params.step[i][1]);
-      			} else {
-        			step = params.step[i];
-      			}
-
-      			waypoints.push({
-      				location: step,
-      				stopover: true
-      			});
-		}
-
-		if(typeof params.end != "object") {
-			var geocodeAsync = function($that) {
-				var geocoder = new google.maps.Geocoder();
-
-		    		geocoder.geocode({
-		    			address  : params.end,
-		    			bounds   : $that.data('googleBound'),
-		    			language : params.langage
-		    		}, function(results, status) {
-	        			if (status == google.maps.GeocoderStatus.OK) {
-	        				var request = {
-	            					origin: params.start,
-	            					destination: results[0].geometry.location,
-	            					travelMode: google.maps.DirectionsTravelMode.DRIVING,
-	            					region: "fr",
-	            					waypoints: waypoints
-		        			};
-
-		        			direction.route(request, function(response, status) {
-	            					if (status == google.maps.DirectionsStatus.OK) {
-	              						way.setDirections(response);
-	            					} else {
-	              						if($that.data('googleDebug'))
-	            							console.error("jQuery googleMap : Unable to find the place asked for the route ("+response+")");
-	            					}
-		        			});
-
-	        			} else {
-	          				if($that.data('googleDebug'))
-	          					console.error("jQuery googleMap : Address not found");
-	        			}
-		    		});
-	    		}($(this));
-		} else {
-			var request = {
-				origin: params.start,
-				destination: new google.maps.LatLng(params.end[0], params.end[1]),
-				travelMode: google.maps.DirectionsTravelMode.DRIVING,
-				region: "fr",
-				waypoints: waypoints
-			};
-
-			direction.route(request, function(response, status) {
-				if (status == google.maps.DirectionsStatus.OK) {
-					way.setDirections(response);
-				} else {
-					if($(this).data('googleDebug'))
-          					console.error("jQuery googleMap : Address not found");
-				}
-			});
-		}
-
-		return this;
-	}
-});
+        //Attach click event to the marker.
+        (function (marker, data) {
+            google.maps.event.addListener(marker, "click", function (e) {
+                //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+    }
+}
