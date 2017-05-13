609c488f7f451926e3e86c47ec3e35c1c28048ce
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-05-13 16:57:48 +0200

    mobile view of recipe page

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$routes = [
+    'home'     => 'default',
+    'convert'  => 'default',
+    'basket'   => 'default',
+    'recipe'   => 'default',
+    'about'    => 'default',
+    'contact'  => 'default',
+    'register' => 'user',
+    'login'    => 'user',
+    'logout'   => 'user',
+];
+
+$db_config = [
+    'name' => 'projet_transversal',
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => ''
+];
