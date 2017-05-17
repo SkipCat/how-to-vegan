cf15b1779abf0b36ee5f668e2572bcf7abd8dbeb
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-05-17 14:12:16 +0200

    anchor system

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -5,4 +5,11 @@ function anchor() {
         $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
         return false;
     });
+    
+    $('.basket').on('click', function () {
+        var page = $(this).attr('href');
+        var speed = 750;
+        $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
+        return false;
+    });
 }
