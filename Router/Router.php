a095beb5187aca9fb2f2ecf7cd66eb62cd78e6c0
Cécile Huguet <cecile.huguet@supinternet.fr>
2017-05-20 22:57:57 +0200

    secure url (redirect to home)

--- Router/Router.php
+++ Router/Router.php
@@ -4,30 +4,29 @@ namespace Router;
 
 use Twig_Environment;
 
-class Router
-{
+class Router {
     private $routes;
     private $twig;
 
-    public function __construct($routes = [], Twig_Environment $twig)
-    {
+    public function __construct($routes = [], Twig_Environment $twig) {
         $this->routes = $routes;
         $this->twig = $twig;
     }
 
-    public function callAction($route)
-    {
-        if (isset($this->routes[$route]))
-        {
+    public function callAction($route) {
+        if (isset($this->routes[$route])) {
             $parts = explode(':', $this->routes[$route]);
-            $controller = $parts[0].'Controller';
-            $method = $parts[1].'Action';
+            $controller = $parts[0] . 'Controller';
+            $method = $parts[1] . 'Action';
         
-            $controller_class = 'Controller\\'.$controller;
+            $controller_class = 'Controller\\' . $controller;
             $controller = new $controller_class($this->twig);
             call_user_func([$controller, $method]);
         }
-        else
-            die('Illegal route');
+        else {
+            //die('Illegal route');            
+            header('Location: ?action=home');
+            exit(0);
+        }
     }
 }
